794bda1c6b02eb50b8b62d5afb0820b05241846e
ZQWhiting <zach.whiting@icloud.com>
2020-07-24 14:17:40 -0600

    finished styling

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,7 +3,6 @@ const { Post, User, Comment } = require('../../models');
 // const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
-	console.log('==================');
 	Post.findAll({
 		order: [['created_at', 'DESC']],
 		attributes: ['id', 'post_text', 'title', 'created_at'],
